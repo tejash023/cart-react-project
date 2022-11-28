df5fc84beee36c61cc659a05dbd0f15e999021ab
Tejashwa Rajvardhan <keshu023@gmail.com>
2022-11-28 23:55:46 +0530

    added cart item compenent

---
+++ src/CartItems.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class CartItem extends React.Component{
+  render(){
+    return(
+      <div className = "cart-item">
+        <div className = "left-block">
+          <img/>
+        </div>
+        <div className='right-block'>
+          <div>Phone</div>
+          <div>Rs 2900</div>
+          <div>Qty: 1</div>
+          <div className='cart-items-action'>
+            {/* Buttons */}
+
+          </div>
+        </div>
+
+      </div>
+    )
+  }
+}
