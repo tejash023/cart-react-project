845495a7a8b3592a10bd0c302adcc5159cc17eb9
Tejashwa Rajvardhan <keshu023@gmail.com>
2022-11-29 00:47:38 +0530

    add state

--- src/CartItems.js
+++ src/CartItems.js
@@ -10,8 +10,13 @@ class CartItem extends React.Component{
       img: ''
     }
   }
+
+  increaseQuantity = () =>{
+    this.state.qty += 1;
+  }
   render(){
-    const {price, title, qty} = this.state;
+    let {price, title, qty} = this.state;
+
     return(
       <div className = "cart-item">
         <div className = "left-block">
@@ -23,9 +28,22 @@ class CartItem extends React.Component{
           <div style={{color:'#777'}}>Qty: {qty}</div>
           <div className='cart-item-actions'>
             {/* Buttons */}
-            <img alt='increase' className='action-icons' src='https://cdn-icons-png.flaticon.com/512/992/992651.png' />
-            <img alt='decrease' className='action-icons' src='https://cdn-icons-png.flaticon.com/512/992/992683.png' />
-            <img alt='delete' className='action-icons' src='https://cdn-icons-png.flaticon.com/512/2907/2907762.png' />
+            <img 
+              alt='increase'
+              className='action-icons' 
+              src='https://cdn-icons-png.flaticon.com/512/992/992651.png'
+              onClick={this.increaseQuantity}
+            />
+            <img 
+              alt='decrease' 
+              className='action-icons' 
+              src='https://cdn-icons-png.flaticon.com/512/992/992683.png' 
+            />
+            <img 
+              alt='delete' 
+              className='action-icons' 
+              src='https://cdn-icons-png.flaticon.com/512/2907/2907762.png' 
+            />
 
           </div>
         </div>
